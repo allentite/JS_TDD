6232b9fd63afd26fa574114ef3b3b507872cb797
allentite <allentite74@gmail.com>
2017-09-19 21:30:27 +0800

    調整變數命名以及格式化程式碼

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -6,16 +6,19 @@ function ShoppingCart(type) {
 ShoppingCart.prototype.Calculate = function (cartItems) {
   var ItemNumber = 0;
   var TotalPrice = 0;
-  cartItems.forEach(function(item) {
+  cartItems.forEach(function (item) {
     ItemNumber++;
     TotalPrice += item;
   });
   if (this.MemberType == "VIP" && TotalPrice >= 500) {
     return TotalPrice * 0.8;
   } else {
-    if (ItemNumber > 3 && TotalPrice >= 1000) { return TotalPrice * 0.85;}
-    else {return TotalPrice; }
-    
+    if (ItemNumber > 3 && TotalPrice >= 1000) {
+      return TotalPrice * 0.85;
+    } else {
+      return TotalPrice;
+    }
+
   }
 }
 
--- tests/shoppingCart.spec.js
+++ tests/shoppingCart.spec.js
@@ -14,12 +14,14 @@ describe('ShoppingCart', () => {
       var expected = 100;
       var actual = 0;
       var shoppingCart = new ShoppingCart();
-      
+
       // Act
       actual = shoppingCart.Calculate(cartPrice);
-      
+
       // Assert
-      actual.should.equal(expected);
+      actual
+        .should
+        .equal(expected);
     });
     it('如果是VIP，購物滿500元，打8折', () => {
       // Arrange
@@ -28,40 +30,45 @@ describe('ShoppingCart', () => {
       var actual = 0;
       var vipShopping = new ShoppingCart("VIP");
 
-      
       // Act
       actual = vipShopping.Calculate(cartPrice);
-      
+
       // Assert
-      actual.should.equal(expected);
+      actual
+        .should
+        .equal(expected);
     });
     it('如果不是VIP會員，則買1000元以及超過三個商品打85折', () => {
       // Arrange
-      var cartPrice = [300,400,500,600];
+      var cartPrice = [300, 400, 500, 600];
       var expected = 1530;
       var actual = 0;
-      var shoppingCart = new ShoppingCart();
-      
+      var nomalShopping = new ShoppingCart();
+
       // Act
-      actual = shoppingCart.Calculate(cartPrice);
-      
+      actual = nomalShopping.Calculate(cartPrice);
+
       // Assert
-      actual.should.equal(expected);
-      
+      actual
+        .should
+        .equal(expected);
+
     });
     it('如果不是VIP會員，則買1000元以及沒超過三個商品照原價', () => {
       // Arrange
-      var cartPrice = [300,400,500];
+      var cartPrice = [300, 400, 500];
       var expected = 1200;
       var actual = 0;
-      var shoppingCart = new ShoppingCart();
-      
+      var nomalShopping = new ShoppingCart();
+
       // Act
-      actual = shoppingCart.Calculate(cartPrice);
-      
+      actual = nomalShopping.Calculate(cartPrice);
+
       // Assert
-      actual.should.equal(expected);
+      actual
+        .should
+        .equal(expected);
     });
   });
-  
+
 });
